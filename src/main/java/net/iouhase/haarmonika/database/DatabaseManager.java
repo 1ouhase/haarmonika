4876bfab368e26758b257ad284c35f5173d21fee
Icerbergsalat <multi1410@gmail.com>
2025-02-26 11:34:08 +0100

    did stuff

--- src/main/java/net/iouhase/haarmonika/database/DatabaseManager.java
+++ src/main/java/net/iouhase/haarmonika/database/DatabaseManager.java
@@ -1,11 +1,9 @@
 package net.iouhase.haarmonika.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseManager {
-    public static Connection Connect() throws SQLException {
+    public static Connection connect() throws SQLException {
         Connection connection;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -14,5 +12,14 @@ public class DatabaseManager {
         }
         connection = DriverManager.getConnection("jdbc:mysql://iouhase.net:3306/hår", "user", "26Uj96MSlPMV4o3aHqIypWcu");
         return connection;
+    }
+    public static void addBooking() {
+        Connection connection = connect();
+    }
+
+    public static void cancelBooking() {
+    }
+
+    public static void editBooking() {
     }
 }
